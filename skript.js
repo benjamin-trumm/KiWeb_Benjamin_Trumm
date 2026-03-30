88f20df615d3566eaeb624c8b1afb68e20a971dc
benjamin-trumm <benjamin.trumm@stud.hslu.ch>
2026-03-30 19:11:17 +0200

    dropdown test

---
+++ skript.js
@@ -0,0 +1,9 @@
+/* Dropdown Menu Toggle */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove("show");
+    }
+  }
+}
