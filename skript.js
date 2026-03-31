4337f34e765783248df9ed07e8577f043928c32c
benjamin-trumm <benjamin.trumm@stud.hslu.ch>
2026-03-31 10:11:03 +0200

    save vvor Neustart

--- skript.js
+++
@@ -1,9 +0,0 @@
-/* Dropdown Menu Toggle */
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    for (let i = 0; i < dropdowns.length; i++) {
-      dropdowns[i].classList.remove("show");
-    }
-  }
-}
